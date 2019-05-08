da5720771299a84cd6a61dbdeabbff18755bc87b
Shamin Ali <shaminali14@gmail.com>
2019-05-08 11:22:39 +0530

    modified app.jsgit add app.js

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-var _express = require('express');
-var _bodyparser = require('body-parser');
+var express = require('express');             //acquiring express
+var bodyParser = require('body-parser');       // acquiring bodyParser
+
+var connection = require('dbconfig/connection');
+var routes = require('controller/routes');
+
+var app = express();
+app.use(bodyParser.urlencoded({extended:true}));    //
+app.use(bodyParser.json());           // 
+
+connection.init();                //to initialize the connection
+routes.configure(app);            // to route the request from client
+
+var server = app.listen(8000, function(){              //start the server at port 8000
+  console.log('Server listening on port ' + server.address().port);
+});
